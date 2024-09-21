0bf3f33cf4cbb8a5d05a4170b180000b656fe6a5
abel <abelgebeyehub@gmail.com>
2024-09-21 18:40:38 -0400

    initial comit

---
+++ script.sh
@@ -0,0 +1,13 @@
+pwd
+echo "hello! I'am using script to crate files."
+mkdir project-1
+cd project-1
+
+touch test-1.txt test-2.txt
+echo "just testing" > test-1.txt
+mkdir project-2
+touch index.html
+mkdir CSS
+cd css
+touch stayle.css
+echo "thank you for now"
